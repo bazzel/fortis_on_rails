db92a977869bfa68a1d0547fb9ccd3d0b45420a7
Patrick Baselier <patrick.baselier@gmail.com>
2010-09-08 13:20:13 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FortisOnRails::Application.config.secret_token = '5432f0fa89e2e0a040bd59d39c103588738f68ea22aebb3e38cc129e18e6521fe87ebe6823b68d2fcb840c1b69261f94f5f89528faf9393363d0418b13058be6'
